57ff9fe8eb3936e304cceda2a1c1213757668bba
gnostertag <ostertag2000@gmail.com>
2023-05-04 10:47:57 -0700

    read functionality

---
+++ src/_tests_/CowIndex.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import CowIndex from "../pages/CowIndex";
+import mockCows from "../mockCows";
+
+describe("<CowIndex/>", () => {
+  it("renders cow cards", () => {
+    render(
+      <BrowserRouter>
+        <CowIndex cows={mockCows} />
+      </BrowserRouter>
+    );
+    mockCows.forEach((cow) => {
+      const cowName = screen.getByText(cow.name);
+      expect(cowName).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/_tests_/CowShow.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import CowShow from "../pages/CowShow";
+import mockCows from "../mockCows";
+
+const renderShow = () => {
+    render(
+        <MemoryRouter initialEntries={['/cowshow/1']}> 
+          <Routes>
+            <Route path='/cowshow/:id' element={<CowShow cows={mockCows}/>}/>
+          </Routes>
+          </MemoryRouter>
+    )
+}
+
+describe("<CowShow/>", () => {
+  it("renders cow card with enjoys", () => {
+    renderShow()
+    expect(screen.getByText(`${mockCows[0].name} likes ${mockCows[0].enjoys}`, {exact: false})).toBeInTheDocument()
+     
+    });
+  });
