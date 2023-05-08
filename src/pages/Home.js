97e36ac0771a8c50cec943397de72df9dce7f475
gnostertag <ostertag2000@gmail.com>
2023-05-08 11:03:59 -0700

    css fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-
   return (
-    
-    <div className="title">Free Range Love</div>
-
-    <div className="title">
-      <h1>Free Range Love</h1>
-      <br />
-      <h2>Where the bovine are SO fine!</h2>
+    <>
+      <div className="title">
+        <h1>Free Range Love</h1>
+        {/* <br /> */}
+        <h2>Where the bovine are SO fine!</h2>
       </div>
+    </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
